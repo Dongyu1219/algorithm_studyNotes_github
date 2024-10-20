85aefba1ab6c0984cf68c3476290d258820a1a07
east_star00 <1219kdk@gmail.com>
2024-10-20 15:39:07 +0900

    quick_sort

--- sort/quick_sort_notes.py
+++ sort/quick_sort_notes.py
@@ -8,12 +8,48 @@ array = [
 
 count = len(array)
 
-def radix_sort(arr):     
-       pass
+def partition(left, right):
+       pi = left                          #가장 왼쪽 값을 피벗으로 삼는다. pi = pivot index
+       pivot = array[pi]                  #pivot = value
+
+       p, q = left, right +1              #후보 선수들 출전 준비
+
+       while True:                        # p와 q 가 역전할 때 까지
+              while True:                 #왼쪽에서 pivot보다 큰 값을 찾을 때 까지
+                     p += 1
+
+                     if p > q: break
+                     if p >right or array[p] >= pivot: break
+                     #왼쪽에서 피벗보다 큰 값을 찾았다
+
+              while True:                 #오른쪽에서 pivot보다 작은 값을 찾을 때 까지
+                     q-=1
+                     if q < p: break
+                     if q<left or array[q] <= pivot: break
+                     #오른쪽에서 pivot보다 작은 값을 찾았다.
+                 
+              if p >= q: break            #p와 q 가 만날때까지 계속 진행한다.
+              array[p], array[q] = array[q], array[p]
+              #array[p]와 array[q]를 교체한다. p이하에는 pivot보다 작은 값만, 
+              #q이상에서는 pivot보다 큰 값만 있다
+              
+       #pivot의 위치를 확정시킨다.
+       #pivot 값은 왼쪽 그룹 중에서 가장 큰 값이므로 q 위치로 옮긴다.
+       #left가 q와 같다면 pivot보다 작은 것이 하나도 없다는 뜻이므로 옮길 필요가 없다.
+       if left !=q:
+              array[left], array[q]= array[q], array[left]
+       return q #q위치를 리턴
+
+
+def quick_sort(left, right):     
+       if left >= right: return           #정렬할 것이 없으면 확정
+       pivot = partition(left, right)     #피벗 위치 결정: partition 함수( pivot = partition(0, count-1) )
+       quick_sort(left, pivot-1)          #pivot 보다 왼쪽 그룹을 다시 퀵소트
+       quick_sort(pivot+1, right)         #pivot 보다 오른쪽 그룹을 다시 퀵소트
 
 def main():
        print(f'before: {array}')
-       raidx_sort(array)
+       quick_sort(0, count-1)
        print(f'after: {array}')
 
 if __name__ == '__main__':
